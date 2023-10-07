ccfe1dd4f94755475ac844d77d4e3aa7121098c9
Davi Vasconcelos <pd350854@gmail.com>
2023-10-07 20:07:33 -0300

    Função de TextToSpeech

--- app/src/main/java/com/example/vamosrachar/MainActivity.kt
+++ app/src/main/java/com/example/vamosrachar/MainActivity.kt
@@ -1,17 +1,27 @@
 package com.example.vamosrachar
 
+import android.annotation.SuppressLint
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
 import android.text.Editable
 import android.widget.EditText
 import android.widget.TextView
 import android.text.TextWatcher
+import android.widget.Button
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
     // Faz com que cada variável seja chamada por um meio, no caso os dois são pelo EditText
     private lateinit var valorTotal: EditText
     private lateinit var nPessoas: EditText
 
+    private lateinit var tts: TextToSpeech
+    @RequiresApi(Build.VERSION_CODES.S)
+    @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -23,6 +33,30 @@ class MainActivity : AppCompatActivity() {
         // Adiciona TextChangedListener aos campos de texto
         valorTotal.addTextChangedListener(textWatcher)
         nPessoas.addTextChangedListener(textWatcher)
+
+        // Variáveis necessárias para o tts
+        var speakButton = findViewById<Button>(R.id.micButton)
+        var el = findViewById<TextView>(R.id.result)
+
+        tts = TextToSpeech(this){status->
+            // Caso a linguagem não seja suportada, ele falará
+            if(status == TextToSpeech.SUCCESS){
+                val result = tts.setLanguage(Locale.getDefault())
+                if (result == TextToSpeech.LANG_MISSING_DATA ||
+                    result == TextToSpeech.LANG_NOT_SUPPORTED){
+                    Toast.makeText(this, "language is not suported", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+
+        // Função principal do tts
+        speakButton.setOnClickListener{
+            if (el.text.toString().trim().isNotEmpty()){
+                tts.speak(el.text.toString().trim(), TextToSpeech.QUEUE_FLUSH, null, null)
+            } else {
+                Toast.makeText(this, "Required", Toast.LENGTH_LONG).show()
+            }
+        }
     }
 
     private val textWatcher = object : TextWatcher {
